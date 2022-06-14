d5328537181201d8884d4bf7c272105fbfe02551
restuarachman <restuarachman@gmail.com>
2022-06-15 01:47:26 +0700

    merge

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,29 @@ Route::resource('tikets', ApiTiketController::class);
 Route::resource('events', ApiEventController::class);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/event', [ApiEventController::class, 'index']);
-    Route::post('/logout', [ApiAuthController::class, 'logout']);
+    Route::get("my-profile",  [ApiUserController::class, 'getUserByToken']);
+    Route::prefix('users')->group(function () {
+        Route::get('/', [ApiUserController::class, 'getAllUsers']);
+        Route::get('/{user_id}', [ApiUserController::class, 'getUser']);
+        Route::put('/{user_id}', [ApiUserController::class, 'updateUser']);
+    });
+    Route::get('/logout', [ApiAuthController::class, 'logout']);
+
+    Route::prefix('events')->group(function () {
+        Route::post('/', [ApiEventController::class, 'createEvent']);
+        Route::put('/{event_id}', [ApiEventController::class, 'updateEvent']);
+        Route::delete('/{event_id}', [ApiEventController::class, 'deleteEvent']);
+    });
+
+    Route::prefix('checkouts')->group(function () {
+        Route::get('/', [ApiCheckoutController::class, 'getAllCheckouts']);
+        Route::post('/', [ApiCheckoutController::class, 'createCheckout']);
+        Route::get('/{checkout_id}', [ApiCheckoutController::class, 'getCheckout']);
+        Route::put('/{checkout_id}', [ApiCheckoutController::class, 'updateCheckout']);
+        Route::get('/{checkout_id}/orders', [ApiOrderController::class, 'getAllOrders']);
+        
+    });
+    Route::post('/orders', [ApiOrderController::class, 'createOrder']);
 });
 
 Route::post('/register', [ApiAuthController::class, 'register']);
