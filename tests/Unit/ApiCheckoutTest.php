81ae879daecaebeff2a1f3c5b0be88679b4848d2
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-06-27 22:46:19 +0700

    add unit test ApiCheckoutTest.php

--- tests/Unit/ApiCheckoutTest.php
+++ tests/Unit/ApiCheckoutTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class ApiCheckoutTest extends TestCase
 {
@@ -11,8 +11,129 @@ class ApiCheckoutTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+
+
+
+
+
+    public function test_getAllCheckouts_positive()
     {
-        $this->assertTrue(true);
+        $data = [
+            "email" => "boltez@gmail.com",
+            "password" => "1234"
+        ];
+
+        $loginResponse = $this->postJson('/api/login', $data);
+        $token = $loginResponse["token"];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/checkouts');
+
+
+
+        $response
+            ->assertStatus(200);
+    }
+
+
+
+    public function test_getAllCheckouts_negative()
+    {
+
+        $response = $this->getJson('/api/checkouts');
+
+        $response
+            ->assertStatus(401);
+    }
+
+
+    public function test_getCheckouts_positive()
+    {
+        $data = [
+            "email" => "boltez@gmail.com",
+            "password" => "1234"
+        ];
+
+        $loginResponse = $this->postJson('/api/login', $data);
+        $token = $loginResponse["token"];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/checkouts/1');
+
+
+
+        $response
+            ->assertStatus(200);
+    }
+
+    public function test_getCheckouts_negative()
+    {
+
+        $response = $this->getJson('/api/checkouts/100000');
+
+        $response
+            ->assertStatus(401);
+    }
+
+    public function test_createCheckout_positive()
+    {
+
+        $data = [
+            "email" => "boltez@gmail.com",
+            "password" => "1234"
+        ];
+
+        $loginResponse = $this->postJson('/api/login', $data);
+        $token = $loginResponse["token"];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/checkouts');
+
+
+
+        $response
+            ->assertStatus(200);
+    }
+
+    public function test_createCheckout_negative()
+    {
+
+        $response = $this->getJson('/api/checkouts');
+
+        $response
+            ->assertStatus(401);
+    }
+
+    public function test_updateCheckout_positive()
+    {
+        $data = [
+            "email" => "boltez@gmail.com",
+            "password" => "1234"
+        ];
+
+        $loginResponse = $this->postJson('/api/login', $data);
+        $token = $loginResponse["token"];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/checkouts/1');
+
+
+
+        $response
+            ->assertStatus(200);
+    }
+
+    public function test_updateCheckout_negative()
+    {
+
+        $response = $this->getJson('/api/checkouts/10000');
+
+        $response
+            ->assertStatus(401);
     }
 }
